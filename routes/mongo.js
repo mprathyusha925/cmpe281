0058e2ed5db41742fa9dbe9e13772ae4036bcc4b
Prathyusha Manusanipalli <Prathyusha Manusanipalli>
2016-05-25 01:06:55 -0700

    mogno

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,3 +1,3 @@
 //exports.URL='ec2-52-86-219-118.compute-1.amazonaws.com';
-exports.URL='ec2-52-38-175-81.us-west-2.compute.amazonaws.com';
+exports.URL='ec2-52-37-230-82.us-west-2.compute.amazonaws.com';
 exports.PORT=8080;
